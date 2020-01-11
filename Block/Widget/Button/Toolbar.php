fd03ba768f147a1809d424f3a0bc1d8a971abdd3
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2020-01-11 12:51:29 +0200

    Added version M 2.3.0 support to SupportTab, Revoked PR 56 due to possible problems with payment method tranfser in live mode.

--- Block/Widget/Button/Toolbar.php
+++ Block/Widget/Button/Toolbar.php
@@ -1,21 +1,40 @@
 <?php
 /**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License
+ * This source file is subject to the Creative Commons License.
  * It is available through the world-wide-web at this URL:
- * https://tldrlegal.com/license/mit-license
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  * If you are unable to obtain it through the world-wide-web, please send an email
- * to support@buckaroo.nl so we can send you a copy immediately.
+ * to servicedesk@tig.nl so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade this module to newer
  * versions in the future. If you wish to customize this module for your
- * needs please contact support@buckaroo.nl for more information.
+ * needs please contact servicedesk@tig.nl for more information.
  *
- * @copyright Copyright (c) Buckaroo B.V.
- * @license   https://tldrlegal.com/license/mit-license
+ * @copyright Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license   http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  */
 
 
@@ -25,22 +44,13 @@ use Magento\Backend\Block\Widget\Button\Toolbar as ToolbarContext;
 use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Backend\Block\Widget\Button\ButtonList;
 
-/**
- * Class Toolbar
- * @package TIG\Buckaroo\Block\Widget\Button
- */
 class Toolbar
 {
-    /**
-     * @var array
-     */
     private $allowedMethods = [
         'tig_buckaroo_afterpay',
         'tig_buckaroo_afterpay2',
-        'tig_buckaroo_afterpay20',
         'tig_buckaroo_payperemail',
         'tig_buckaroo_creditcard',
-        'tig_buckaroo_creditcards',
         'tig_buckaroo_ideal',
         'tig_buckaroo_idealprocessing',
         'tig_buckaroo_mrcash',
@@ -51,15 +61,7 @@ class Toolbar
         'tig_buckaroo_transfer',
         'tig_buckaroo_paymentguarantee',
         'tig_buckaroo_eps',
-        'tig_buckaroo_giropay',
-        'tig_buckaroo_kbc',
-        'tig_buckaroo_klarna',
-        'tig_buckaroo_emandate',
-        'tig_buckaroo_applepay',
-        'tig_buckaroo_capayablein3',
-        'tig_buckaroo_capayablepostpay',
-        'tig_buckaroo_alipay',
-        'tig_buckaroo_wechatpay',
+        'tig_buckaroo_giropay'
     ];
 
     /**
@@ -73,42 +75,10 @@ class Toolbar
         \Magento\Framework\View\Element\AbstractBlock $context,
         \Magento\Backend\Block\Widget\Button\ButtonList $buttonList
     ) {
-        if ($this->_isOrderInvoiceView($context)) {
-            return $this->_creditMemoNotAllowed($context, $buttonList);
+        if (!$context instanceof \Magento\Sales\Block\Adminhtml\Order\Invoice\View) {
+            return [$context, $buttonList];
         }
 
-        return [$context, $buttonList];
-    }
-
-    /**
-     * @param $context
-     * @return bool
-     */
-    private function _isOrderView($context)
-    {
-        if ($context instanceof \Magento\Sales\Block\Adminhtml\Order\View) {
-            return true;
-        }
-    }
-
-    /**
-     * @param $context
-     * @return bool
-     */
-    private function _isOrderInvoiceView($context)
-    {
-        if ($context instanceof \Magento\Sales\Block\Adminhtml\Order\Invoice\View) {
-            return true;
-        }
-    }
-
-    /**
-     * @param $context
-     * @param $buttonList
-     * @return array
-     */
-    private function _creditMemoNotAllowed($context, $buttonList)
-    {
         $orderPayment = $context->getInvoice()->getOrder();
         $paymentMethod = $orderPayment->getPayment()->getMethod();
 
@@ -117,7 +87,7 @@ class Toolbar
                 'Cannot Refund via Magento Backend. ' .
                 'Partial refunds combined with a payment fee can only be refunded via the Buckaroo Payment Plaza, ' .
                 'see also the ' .
-                '<a href="https://confluence.tig.nl/x/L4aC" target="_blank">KB article</a>.<br>' .
+                '<a href="http://servicedesk.tig.nl/hc/nl/articles/217984838" target="_blank">KB article</a>.<br>' .
                 '<a href="https://plaza.buckaroo.nl" target="_blank">' .
                 'Open a new window to the Buckaroo Payment Plaza</a>.'
             );
@@ -140,6 +110,8 @@ class Toolbar
 
             $buttonList->update('capture', 'onclick', $onClick);
         }
+
+
 
         return [$context, $buttonList];
     }
--- Test/Unit/Model/ConfigProvider/AccountTest.php
+++ Test/Unit/Model/ConfigProvider/AccountTest.php
@@ -1,21 +1,40 @@
 <?php
 /**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License
+ * This source file is subject to the Creative Commons License.
  * It is available through the world-wide-web at this URL:
- * https://tldrlegal.com/license/mit-license
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  * If you are unable to obtain it through the world-wide-web, please send an email
- * to support@buckaroo.nl so we can send you a copy immediately.
+ * to servicedesk@tig.nl so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade this module to newer
  * versions in the future. If you wish to customize this module for your
- * needs please contact support@buckaroo.nl for more information.
+ * needs please contact servicedesk@tig.nl for more information.
  *
- * @copyright Copyright (c) Buckaroo B.V.
- * @license   https://tldrlegal.com/license/mit-license
+ * @copyright Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license   http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  */
 namespace TIG\Buckaroo\Test\Unit\Model\ConfigProvider;
 
@@ -32,11 +51,10 @@ class AccountTest extends BaseTest
     public function testGetConfig()
     {
         $expectedKeys = [
-            'active', 'secret_key', 'merchant_key', 'merchant_guid', 'transaction_label', 'certificate_file',
-            'order_confirmation_email', 'invoice_email', 'success_redirect', 'failure_redirect', 'cancel_on_failed',
-            'digital_signature', 'debug_types', 'debug_email', 'limit_by_ip', 'fee_percentage_mode',
-            'payment_fee_label', 'order_status_new', 'order_status_pending', 'order_status_success',
-            'order_status_failed', 'create_order_before_transaction'
+            'active', 'secret_key', 'merchant_key', 'transaction_label', 'certificate_file', 'order_confirmation_email',
+            'invoice_email', 'success_redirect', 'failure_redirect', 'cancel_on_failed', 'digital_signature',
+            'debug_types', 'debug_email', 'limit_by_ip', 'fee_percentage_mode', 'payment_fee_label', 'order_status_new',
+            'order_status_pending', 'order_status_success', 'order_status_failed', 'create_order_before_transaction'
         ];
 
         $instance = $this->getInstance();
