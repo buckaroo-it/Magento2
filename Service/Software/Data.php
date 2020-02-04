2b830befc9505047b0078426fbbb141f814fec27
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2020-02-04 10:15:12 +0200

    version change

--- Service/Software/Data.php
+++ Service/Software/Data.php
@@ -31,7 +31,7 @@ class Data
     const MODULE_CODE = 'Buckaroo_Magento2';
 
     /** Version of Module */
-    const BUCKAROO_VERSION = '1.16.1';
+    const BUCKAROO_VERSION = '1.17.0';
 
     /** @var ProductMetadataInterface */
     private $productMetadata;
