a4a804a84866625a81963e8455dcbda3d8eb1f94
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2020-08-18 13:46:03 +0300

    Magento 2 - klarnakp / fix calendar month issue

--- view/frontend/web/js/view/payment/method-renderer/klarnakp.js
+++ view/frontend/web/js/view/payment/method-renderer/klarnakp.js
@@ -235,6 +235,7 @@ define(
                      */
 
                     var runValidation = function () {
+                        window.buckarooKlarnaDob = this.dateValidate();
                         $('.' + this.getCode() + ' [data-validate]').filter(':not([name*="agreement"])').valid();
                         this.selectPaymentMethod();
                     };
