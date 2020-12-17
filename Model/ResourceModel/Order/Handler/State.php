712942aae224fee23230d726999f45da551d6f8a
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2020-12-17 17:16:16 +0200

    BP-542 Magento 2 - B2B PPE flow fix

---
+++ Model/ResourceModel/Order/Handler/State.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Buckaroo\Magento2\Model\ResourceModel\Order\Handler;
+
+use Magento\Sales\Model\Order;
+use Buckaroo\Magento2\Logging\Log;
+use Buckaroo\Magento2\Model\ConfigProvider\Method\Factory;
+
+class State extends \Magento\Sales\Model\ResourceModel\Order\Handler\State
+{
+    /**
+     * @var Factory
+     */
+    public $configProviderMethodFactory;
+
+    public function __construct(
+        Factory $configProviderMethodFactory,
+        Log $logging
+    )
+    {
+        $this->configProviderMethodFactory = $configProviderMethodFactory;
+        $this->logging = $logging;
+    }
+
+    public function check(Order $order)
+    {
+        if ($order->getPayment() && $order->getPayment()->getMethodInstance()->getCode() == 'buckaroo_magento2_payperemail') {
+            $config = $this->configProviderMethodFactory->get(\Buckaroo\Magento2\Model\Method\PayPerEmail::PAYMENT_METHOD_CODE);
+            if ($config->getEnabledB2B()) {
+                if ($order->getState() == Order::STATE_PROCESSING) {
+                    if ($order->getInvoiceCollection() && $order->getInvoiceCollection()->getFirstItem()) {
+                        if ($order->getInvoiceCollection()->getFirstItem()->getState() == 1) {
+                            $this->logging->addDebug(__METHOD__ . '|10|');
+                            return $this;
+                        }
+                    }
+                }
+            }
+        }
+
+        return parent::check($order);
+    }
+}
