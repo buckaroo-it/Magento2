ec9d581f1b031a76d945e97f46764070a64794f3
Florinel Chis <florinel.chis@gmail.com>
2021-04-09 20:36:54 +0300

    BP-720 Salable qty issue

--- Observer/HandleFailedQuoteOrder.php
+++ Observer/HandleFailedQuoteOrder.php
@@ -20,8 +20,18 @@
 
 namespace Buckaroo\Magento2\Observer;
 
+use Buckaroo\Magento2\Model\Session as BuckarooSession;
+
 class HandleFailedQuoteOrder implements \Magento\Framework\Event\ObserverInterface
 {
+    protected $buckarooSession;
+
+    public function __construct(
+        BuckarooSession $buckarooSession
+    ) {
+        $this->buckarooSession = $buckarooSession;
+    }
+
     /**
      * @param \Magento\Framework\Event\Observer $observer
      * @return void
@@ -60,6 +70,7 @@ class HandleFailedQuoteOrder implements \Magento\Framework\Event\ObserverInterfa
             }
 
             try {
+                $this->buckarooSession->setData('flagHandleFailedQuote', 1);
                 $order->cancel();
                 $order->save();
             } catch (\Exception $e) {
