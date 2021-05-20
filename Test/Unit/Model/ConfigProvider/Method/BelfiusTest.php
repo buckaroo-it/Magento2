2d2287809c428b08c14bd5feb578863ee2eba565
Florinel Chis <florinel.chis@gmail.com>
2021-05-20 17:27:50 +0300

    BP-816 unit tests - config provider

---
+++ Test/Unit/Model/ConfigProvider/Method/BelfiusTest.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * It is available through the world-wide-web at this URL:
+ * https://tldrlegal.com/license/mit-license
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to support@buckaroo.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact support@buckaroo.nl for more information.
+ *
+ * @copyright Copyright (c) Buckaroo B.V.
+ * @license   https://tldrlegal.com/license/mit-license
+ */
+namespace Buckaroo\Magento2\Test\Unit\Model\ConfigProvider\Method;
+
+use Magento\Store\Model\ScopeInterface;
+use Buckaroo\Magento2\Helper\PaymentFee;
+use Buckaroo\Magento2\Test\BaseTest;
+use Buckaroo\Magento2\Model\ConfigProvider\Method\Belfius;
+use Buckaroo\Magento2\Model\Method\Belfius as BelfiusMethod;
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+
+class BelfiusTest extends BaseTest
+{
+    protected $instanceClass = Belfius::class;
+
+    public function getConfigProvider()
+    {
+        return [
+            'active' => [
+                true,
+                [
+                    'payment' => [
+                        'buckaroo' => [
+                            'belfius' => [
+                                'paymentFeeLabel' => 'Belfius Fee',
+                                'allowedCurrencies' => ['EUR'],
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @param $expected
+     *
+     * @covers ::getConfig
+     * @dataProvider getConfigProvider
+     */
+    public function testGetConfig($active,$expected)
+    {
+        $scopeConfigMock = $this->getFakeMock(ScopeConfigInterface::class)
+            ->setMethods(['getValue'])
+            ->getMockForAbstractClass();
+
+        $scopeConfigMock->expects($this->atLeastOnce())
+            ->method('getValue')
+            ->withConsecutive(
+                [Belfius::XPATH_ALLOWED_CURRENCIES, ScopeInterface::SCOPE_STORE, null],
+                [Belfius::XPATH_BELFIUS_PAYMENT_FEE_LABEL, ScopeInterface::SCOPE_STORE, null],
+            )
+            ->willReturnOnConsecutiveCalls('EUR', 'Belfius Fee');
+
+        $paymentFeeMock = $this->getFakeMock(PaymentFee::class)->setMethods(['getBuckarooPaymentFeeLabel'])->getMock();
+        $paymentFeeMock->method('getBuckarooPaymentFeeLabel')->with(BelfiusMethod::PAYMENT_METHOD_CODE)->willReturn('Belfius Fee');
+
+        $instance = $this->getInstance(['scopeConfig' => $scopeConfigMock, 'paymentFeeHelper' => $paymentFeeMock]);
+
+        $result = $instance->getConfig();
+        $fh = fopen('/Users/florinel/buckaroo/m23/result.txt','w+');
+        fwrite($fh,print_r($result,true));
+        fwrite($fh,print_r($expected,true));
+        fclose($fh);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function getPaymentFeeProvider()
+    {
+        return [
+            'null value' => [
+                null,
+                false
+            ],
+            'false value' => [
+                false,
+                false
+            ],
+            'empty int value' => [
+                0,
+                false
+            ],
+            'empty float value' => [
+                0.00,
+                false
+            ],
+            'empty string value' => [
+                '',
+                false
+            ],
+            'int value' => [
+                1,
+                1
+            ],
+            'float value' => [
+                2.34,
+                2.34
+            ],
+            'string value' => [
+                '5.67',
+                5.67
+            ],
+        ];
+    }
+
+    /**
+     * @param $value
+     * @param $expected
+     *
+     * @covers ::getPaymentFee
+     * @dataProvider getPaymentFeeProvider
+     */
+    public function testGetPaymentFee($value, $expected)
+    {
+        $scopeConfigMock = $this->getFakeMock(ScopeConfigInterface::class)
+            ->setMethods(['getValue'])
+            ->getMockForAbstractClass();
+
+        $scopeConfigMock->expects($this->once())
+            ->method('getValue')
+            ->with(Belfius::XPATH_BELFIUS_PAYMENT_FEE, ScopeInterface::SCOPE_STORE)
+            ->willReturn($value);
+
+        $instance = $this->getInstance(['scopeConfig' => $scopeConfigMock]);
+        $result = $instance->getPaymentFee();
+
+        $this->assertEquals($expected, $result);
+    }
+}
