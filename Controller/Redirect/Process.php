fd4bc4a57c4ce2678747665f74592ddf2806d236
Sergey Saranchuk <sergey.saranchuk@hys-enterprise.com>
2021-06-29 10:03:06 +0300

    BP-933  No second chance email sended on pending orders

--- Controller/Redirect/Process.php
+++ Controller/Redirect/Process.php
@@ -301,6 +301,7 @@ class Process extends \Magento\Framework\App\Action\Action
                         )
                     );
                     $this->logger->addDebug(__METHOD__ . '|5|');
+                    $this->secondChance();
                     return $this->_redirect('/');
                     // $this->redirectFailure();
                 }
