77e15f8e4b224a2ad9e4f23c4703c070cf23c155
Sergey Saranchuk <sergey.saranchuk@hys-enterprise.com>
2021-08-09 17:35:03 +0300

    BP-1010 Billink JS error

--- view/frontend/web/js/view/payment/method-renderer/billink.js
+++ view/frontend/web/js/view/payment/method-renderer/billink.js
@@ -208,7 +208,7 @@ define(
 
                     quote.billingAddress.subscribe(
                         function(newAddress) {
-                            this.businessMethod = quote.billingAddress().company ? BUSINESS_METHOD_B2B : BUSINESS_METHOD_B2C;
+                            this.businessMethod = quote.billingAddress() && quote.billingAddress().company ? BUSINESS_METHOD_B2B : BUSINESS_METHOD_B2C;
 
                             if (this.getCode() !== this.isChecked() ||
                                 !newAddress ||
