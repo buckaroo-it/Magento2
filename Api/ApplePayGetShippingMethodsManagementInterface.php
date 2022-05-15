0ed053cf712090d15c88ffb6b7ba4676327c615c
Florinel Chis <florinel.chis@gmail.com>
2022-05-16 02:04:09 +0300

    BP-1500 Apple Pay refactor - use cart hash

---
+++ Api/ApplePayGetShippingMethodsManagementInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buckaroo\Magento2\Api;
+
+interface ApplePayGetShippingMethodsManagementInterface
+{
+
+    /**
+     * POST for ApplePayGetShippingMethods api     
+     * @return string
+     */
+    public function postApplePayGetShippingMethods();
+}
