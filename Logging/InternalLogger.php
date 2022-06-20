eef598fd0a723491a9b33a23a011a9971fb2c41d
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-06-20 07:46:46 +0000

    fix interanl logger error

--- Logging/InternalLogger.php
+++ Logging/InternalLogger.php
@@ -19,8 +19,13 @@
  */
 namespace Buckaroo\Magento2\Logging;
 
+use DateTimeZone;
 use Monolog\Logger;
 
 class InternalLogger extends Logger {
 
+    public function __construct(string $name, array $handlers = [], array $processors = [], ?DateTimeZone $timezone = null)
+    {
+        parent::__construct($name, $handlers, $processors, $timezone);
+    }
 }
