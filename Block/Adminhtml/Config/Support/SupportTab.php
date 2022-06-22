173133371187d373ee6c3278219cf3039e0a52b6
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-06-22 06:41:45 +0000

    changed php version array

--- Block/Adminhtml/Config/Support/SupportTab.php
+++ Block/Adminhtml/Config/Support/SupportTab.php
@@ -31,8 +31,7 @@ class SupportTab extends \Magento\Framework\View\Element\Template implements Ren
     /** @var array  */
     private $phpVersionSupport = [
         '2.3' => ['7.3' => ['+'], '7.4' => ['+']],
-        '2.4' => ['7.4' => ['+']],
-        '2.4' => ['8.1' => ['+']],
+        '2.4' => ['7.4' => ['+'], '8.1' => ['+']],
     ];
 
     /**
