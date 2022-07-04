1afbfa42b0a341a196ed96714ea7325842329172
Lucian Turiac <lucian.turiac@arnia.ro>
2022-07-04 19:06:35 +0300

    Refactoring - Add isAvailable in Adapter (first solution)

--- Gateway/Validator/AreaCodeValidator.php
+++ Gateway/Validator/AreaCodeValidator.php
@@ -2,14 +2,58 @@
 
 namespace Buckaroo\Magento2\Gateway\Validator;
 
+use Buckaroo\Magento2\Model\ConfigProvider\Factory as ConfigProviderFactory;
 use Magento\Payment\Gateway\Validator\AbstractValidator;
 use Magento\Payment\Gateway\Validator\ResultInterface;
+use Magento\Payment\Gateway\Validator\ResultInterfaceFactory;
+use Buckaroo\Magento2\Model\ConfigProvider\Method\AbstractConfigProvider;
+use Magento\Framework\App\State;
+use Magento\Framework\App\Area;
 
 class AreaCodeValidator extends AbstractValidator
 {
+    private ConfigProviderFactory $configProviderFactory;
+    private State $state;
+
+    /**
+     * @param ResultInterfaceFactory $resultFactory
+     * @param ConfigProviderFactory $configProviderFactory
+     */
+    public function __construct(
+        ResultInterfaceFactory $resultFactory,
+        ConfigProviderFactory  $configProviderFactory,
+        State $state
+    ) {
+        $this->configProviderFactory = $configProviderFactory;
+        $this->state = $state;
+        parent::__construct($resultFactory);
+    }
 
     public function validate(array $validationSubject)
     {
-        // TODO: Implement validate() method.
+        $isValid = true;
+
+        if (!isset($validationSubject['payment'])) {
+            return $this->createResult(
+                false,
+                [__('Payment method instance does not exist')]
+            );
+        }
+
+        $paymentMethodInstance = $validationSubject['payment'];
+        $areaCode = $this->state->getAreaCode();
+        /**
+         * @var AbstractConfigProvider
+         */
+        $config = $this->configProviderFactory->get($paymentMethodInstance->buckarooPaymentMethodCode);
+        if (Area::AREA_ADMINHTML === $areaCode
+            && $config->getValue('available_in_backend') !== null
+            && $config->getValue('available_in_backend') == 0
+        ) {
+            $isValid = false;
+        }
+
+        return $this->createResult($isValid);
+
     }
 }
--- Model/Method/BuckarooAdapter.php
+++ Model/Method/BuckarooAdapter.php
@@ -11,15 +11,23 @@ use Magento\Framework\Event\ManagerInterface;
 use Magento\Payment\Model\InfoInterface;
 use Magento\Sales\Api\Data\OrderPaymentInterface;
 use Psr\Log\LoggerInterface;
+use Magento\Quote\Api\Data\CartInterface;
+use Magento\Quote\Model\Quote;
+use Magento\Framework\App\State;
+use Magento\Framework\App\Area;
+use Magento\Framework\Exception\LocalizedException;
 
 class BuckarooAdapter extends \Magento\Payment\Model\Method\Adapter
 {
-    const PAYMENT_FROM = 'buckaroo_payment_from';
-
     /**
      * @var bool
      */
     public bool $usesRedirect = true;
+
+    /**
+     * @var string
+     */
+    public $buckarooPaymentMethodCode;
 
     /**
      * @var \Magento\Framework\ObjectManagerInterface
@@ -31,21 +39,37 @@ class BuckarooAdapter extends \Magento\Payment\Model\Method\Adapter
      */
     protected $request;
 
+    private State $state;
+
+    /**
+     * @var \Magento\Developer\Helper\Data
+     */
+    protected $developmentHelper;
+
+    /**
+     * @var \Buckaroo\Magento2\Model\ConfigProvider\Factory
+     */
+    public $configProviderFactory;
+
     public function __construct(
-        ManagerInterface $eventManager,
-        ValueHandlerPoolInterface $valueHandlerPool,
-        PaymentDataObjectFactory $paymentDataObjectFactory,
-        $code,
-        $formBlockType,
-        $infoBlockType,
-        \Magento\Framework\ObjectManagerInterface $objectManager,
-        \Magento\Framework\App\RequestInterface $request = null,
-        CommandPoolInterface $commandPool = null,
-        ValidatorPoolInterface $validatorPool = null,
-        CommandManagerInterface $commandExecutor = null,
-        LoggerInterface $logger = null,
-        bool $usesRedirect = true
-    ) {
+        ManagerInterface                                $eventManager,
+        ValueHandlerPoolInterface                       $valueHandlerPool,
+        PaymentDataObjectFactory                        $paymentDataObjectFactory,
+                                                        $code,
+                                                        $formBlockType,
+                                                        $infoBlockType,
+        \Magento\Framework\ObjectManagerInterface       $objectManager,
+        State                                           $state,
+        \Magento\Developer\Helper\Data                  $developmentHelper,
+        \Magento\Framework\App\RequestInterface         $request = null,
+        CommandPoolInterface                            $commandPool = null,
+        ValidatorPoolInterface                          $validatorPool = null,
+        CommandManagerInterface                         $commandExecutor = null,
+        LoggerInterface                                 $logger = null,
+        \Buckaroo\Magento2\Model\ConfigProvider\Factory $configProviderFactory = null,
+        bool                                            $usesRedirect = true
+    )
+    {
         parent::__construct(
             $eventManager,
             $valueHandlerPool,
@@ -59,14 +83,136 @@ class BuckarooAdapter extends \Magento\Payment\Model\Method\Adapter
             $logger
         );
 
+        $this->buckarooPaymentMethodCode = $code;
         $this->objectManager = $objectManager;
         $this->request = $request;
+        $this->state = $state;
+        $this->developmentHelper = $developmentHelper;
         $this->usesRedirect = $usesRedirect;
+        $this->configProviderFactory = $configProviderFactory;
     }
+
+    /**
+     * @inheritdoc
+     * This is a temporary workaround for https://github.com/magento/magento2/issues/33869.
+     * It sets the info instance before the method gets executed. Otherwise, the validator doesn't get called
+     * correctly.
+     */
+    public function isAvailable(CartInterface $quote = null)
+    {
+        if (null == $quote) {
+            return false;
+        }
+        /**
+         * @var \Buckaroo\Magento2\Model\ConfigProvider\Account $accountConfig
+         */
+        $accountConfig = $this->configProviderFactory->get('account');
+        if ($accountConfig->getActive() == 0) {
+            return false;
+        }
+
+        $areaCode = $this->state->getAreaCode();
+        if ('adminhtml' === $areaCode
+            && $this->getConfigData('available_in_backend') !== null
+            && $this->getConfigData('available_in_backend') == 0
+        ) {
+            return false;
+        }
+
+        if (!$this->isAvailableBasedOnIp($accountConfig, $quote)) {
+            return false;
+        }
+
+        if (!$this->isAvailableBasedOnAmount($quote)) {
+            return false;
+        }
+
+        if (!$this->isAvailableBasedOnCurrency($quote)) {
+            return false;
+        }
+
+        $this->setInfoInstance($quote->getPayment());
+        return parent::isAvailable($quote);
+    }
+
+    /**
+     * Check if this payment method is limited by IP.
+     *
+     * @param \Buckaroo\Magento2\Model\ConfigProvider\Account $accountConfig
+     * @param \Magento\Quote\Api\Data\CartInterface $quote
+     *
+     * @return bool
+     */
+    protected function isAvailableBasedOnIp(
+        \Buckaroo\Magento2\Model\ConfigProvider\Account $accountConfig,
+        \Magento\Quote\Api\Data\CartInterface           $quote = null
+    )
+    {
+        $methodValue = $this->getConfigData('limit_by_ip');
+        if ($accountConfig->getLimitByIp() == 1 || $methodValue == 1) {
+            $storeId = $quote ? $quote->getStoreId() : null;
+            $isAllowed = $this->developmentHelper->isDevAllowed($storeId);
+
+            if (!$isAllowed) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if the grand total exceeds the maximum allowed total.
+     *
+     * @param \Magento\Quote\Api\Data\CartInterface $quote
+     *
+     * @return bool
+     */
+    protected function isAvailableBasedOnAmount(\Magento\Quote\Api\Data\CartInterface $quote = null)
+    {
+        $storeId = $quote->getStoreId();
+        $maximum = $this->getConfigData('max_amount', $storeId);
+        $minimum = $this->getConfigData('min_amount', $storeId);
+
+        /**
+         * @var \Magento\Quote\Model\Quote $quote
+         */
+        $total = $quote->getGrandTotal();
+
+        if ($total < 0.01) {
+            return false;
+        }
+
+        if ($maximum !== null && $total > $maximum) {
+            return false;
+        }
+
+        if ($minimum !== null && $total < $minimum) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param \Magento\Quote\Api\Data\CartInterface $quote
+     *
+     * @return bool
+     */
+    protected function canUseForCurrency(\Magento\Quote\Api\Data\CartInterface $quote = null)
+    {
+        $allowedCurrenciesRaw = $this->getConfigData('allowed_currencies');
+        $allowedCurrencies = explode(',', (string)$allowedCurrenciesRaw);
+
+        $currentCurrency = $quote->getCurrency()->getQuoteCurrencyCode();
+
+        return $allowedCurrenciesRaw === null || in_array($currentCurrency, $allowedCurrencies);
+    }
+
 
     /**s
      * @param OrderPaymentInterface|InfoInterface $payment
-     * @param array                               $postData
+     * @param array $postData
      *
      * @return bool
      */
@@ -77,7 +223,7 @@ class BuckarooAdapter extends \Magento\Payment\Model\Method\Adapter
 
     /**
      * @param OrderPaymentInterface|InfoInterface $payment
-     * @param array                               $postData
+     * @param array $postData
      */
     public function processCustomPostData($payment, $postData)
     {
@@ -85,7 +231,7 @@ class BuckarooAdapter extends \Magento\Payment\Model\Method\Adapter
     }
 
     /**
-     * @param  \Magento\Framework\DataObject $data
+     * @param \Magento\Framework\DataObject $data
      *
      * @return array
      */
