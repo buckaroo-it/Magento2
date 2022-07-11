810faf4d4feeaec7ea50e2fe1a7769fb89dbbf68
Lucian Turiac <lucian.turiac@arnia.ro>
2022-07-11 17:55:10 +0300

    Refactoring - Use Config instead .env

--- Model/Adapter/BuckarooAdapter.php
+++ Model/Adapter/BuckarooAdapter.php
@@ -3,6 +3,8 @@
 namespace Buckaroo\Magento2\Model\Adapter;
 
 use Buckaroo\Buckaroo;
+use Buckaroo\Config\Config;
+use Buckaroo\Magento2\Model\ConfigProvider\Account;
 use Dotenv\Dotenv;
 
 class BuckarooAdapter
@@ -12,11 +14,20 @@ class BuckarooAdapter
      */
     private Buckaroo $buckaroo;
 
-    public function __construct()
+    /**
+     * @var Account
+     */
+    private $configProviderAccount;
+
+    public function __construct(Account $configProviderAccount)
     {
         $dotenv = Dotenv::createImmutable(__DIR__ );
         $dotenv->load();
-        $this->buckaroo = new Buckaroo($_ENV['BPE_WEBSITE_KEY'], $_ENV['BPE_SECRET_KEY'], $_ENV['BPE_MODE']);
+        $this->configProviderAccount = $configProviderAccount;
+        $websiteKey = $this->configProviderAccount->getMerchantKey();
+        $secretKey = $this->configProviderAccount->getSecretKey();
+        $envMode = $this->configProviderAccount->getActive() == 2 ? Config::LIVE_MODE : Config::TEST_MODE;
+        $this->buckaroo = new Buckaroo($websiteKey, $secretKey, $envMode);
     }
 
     public function pay($method, $data) {
