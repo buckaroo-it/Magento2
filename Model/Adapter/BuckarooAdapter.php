12e7a623e24e73080d4797e9ea2d98dbe7336076
Lucian Turiac <lucian.turiac@arnia.ro>
2022-08-22 10:16:02 +0300

    Refactoring - Add EPS payment method

--- Model/Adapter/BuckarooAdapter.php
+++ Model/Adapter/BuckarooAdapter.php
@@ -73,6 +73,6 @@ class BuckarooAdapter
 
     private function getMethodName($method)
     {
-        return isset($this->mapPaymentMethods[$method]) ? $this->mapPaymentMethods[$method] : $method;
+        return $this->mapPaymentMethods[$method] ?? $method;
     }
 }
