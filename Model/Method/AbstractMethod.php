91120dfdd1e898da8909bcffe6120782246cffdb
Ivascu Madalin <madalinivascu@gmail.com>
2022-11-15 11:25:43 +0200

    BP-2102 Rejection error not show in Magento (#624)
    Co-authored-by: Ivascu Madalin <madalin.ivascu@arnia.ro>

--- Model/Method/AbstractMethod.php
+++ Model/Method/AbstractMethod.php
@@ -791,7 +791,10 @@ abstract class AbstractMethod extends \Magento\Payment\Model\Method\AbstractMeth
         $billingCountry      = $this->payment->getOrder()->getBillingAddress()->getCountryId();
 
         if($responseCode == 491) {
-            return $this->getFirstError($transactionResponse);
+            $errorMessage =  $this->getFirstError($transactionResponse);
+            if(strlen(trim($errorMessage)) > 0) {
+                return $errorMessage;
+            }
         }
 
         $method = null;
@@ -811,8 +814,12 @@ abstract class AbstractMethod extends \Magento\Payment\Model\Method\AbstractMeth
             $message       = strlen($methodMessage) > 0 ? $methodMessage : $message;
         }
 
-        if (isset($transactionResponse->Status->SubCode->_)) {
-            $message = $transactionResponse->Status->SubCode->_;
+        if (
+            isset($transactionResponse->Status->SubCode->_) &&
+            is_string($transactionResponse->Status->SubCode->_) &&
+            strlen(trim($transactionResponse->Status->SubCode->_)) > 0
+        ) {
+                $message = $transactionResponse->Status->SubCode->_;
         }
 
         $fraudMessage = $this->getFailureMessageOnFraud($transactionResponse);
