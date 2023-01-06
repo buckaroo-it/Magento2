32be10d4a260c4d34265e808c8ca79f8c69eb37c
Lucian Turiac <lucian.turiac@arnia.ro>
2023-01-06 23:22:47 +0200

    BP-2090 - Feedback Marius - Fixes

--- Gateway/Request/LocaleDataBuilder.php
+++ Gateway/Request/LocaleDataBuilder.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Buckaroo\Magento2\Gateway\Request;
 
-use Magento\Sales\Api\Data\OrderAddressInterface;
-
 class LocaleDataBuilder extends AbstractDataBuilder
 {
     public function build(array $buildSubject): array
@@ -19,13 +17,11 @@ class LocaleDataBuilder extends AbstractDataBuilder
     {
         $country = $this->getOrder()->getBillingAddress()->getCountryId();
 
-        if ($country == 'CN') {
-            $localeCode = 'zh-CN';
-        } elseif ($country == 'TW') {
-            $localeCode = 'zh-TW';
-        } else {
-            $localeCode = 'en-US';
-        }
-        return $localeCode;
+        $map = [
+            'CN' => 'zh-CN',
+            'TW' => 'zh-TW'
+        ];
+
+        return $map[$country] ?? 'en-US';
     }
 }
