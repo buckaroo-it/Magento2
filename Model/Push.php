f50eb43195ec03e30053aa9c90d7fe8846f20007
Ivascu Madalin <madalinivascu@gmail.com>
2023-03-28 16:56:01 +0300

    BP-2408 Cancel Magento order when group transaction is cancelled at Buckaroo (#691)
    Co-authored-by: Ivascu Madalin <madalin.ivascu@arnia.ro>

--- Model/Push.php
+++ Model/Push.php
@@ -237,6 +237,10 @@ class Push implements PushInterface
 
 
         if ($this->isGroupTransactionInfo()) {
+            if($this->isCanceledGroupTransaction()) {
+                $this->cancelGroupTransactionOrder();
+                return true;
+            }
             if ($this->isGroupTransactionFailed()) {
                 $this->savePartGroupTransaction();
             } else {
@@ -1986,7 +1990,7 @@ class Push implements PushInterface
      *
      * @return void
      */
-    protected function cancelOrder(string $reservedOrderId)
+    protected function cancelOrder(string $reservedOrderId, $historyComment = 'Giftcard has expired')
     {
         $order = $this->order->loadByIncrementId($reservedOrderId);
 
@@ -2005,7 +2009,7 @@ class Push implements PushInterface
             $orderManagement->cancel($order->getEntityId());
 
             $order->addCommentToStatusHistory(
-                __('Giftcard has expired')
+                __($historyComment)
             )
             ->setIsCustomerNotified(false)
             ->setEntityName('invoice')
@@ -2046,5 +2050,40 @@ class Push implements PushInterface
         return $order;
 
 
+    }
+
+    /**
+     * Cancel order when group transaction is canceled
+     *
+     * @return void
+     */
+    public function cancelGroupTransactionOrder()
+    {
+        if(
+            isset($this->postData['brq_invoicenumber']) &&
+            is_string($this->postData['brq_invoicenumber'])
+        ) {
+            $this->cancelOrder(
+                $this->postData['brq_invoicenumber'],
+                'Inline giftcard order was canceled'
+            );
+        }
+    }
+
+    /**
+     * Check if the request is a canceled group transaction
+     *
+     * @return boolean
+     */
+    public function isCanceledGroupTransaction()
+    {
+        return $this->hasPostData(
+            'brq_transaction_type',
+            self::BUCK_PUSH_GROUPTRANSACTION_TYPE
+        ) &&
+        $this->hasPostData(
+            'brq_statuscode',
+            $this->helper->getStatusCode('BUCKAROO_MAGENTO2_STATUSCODE_CANCELLED_BY_USER')
+        );
     }
 }
