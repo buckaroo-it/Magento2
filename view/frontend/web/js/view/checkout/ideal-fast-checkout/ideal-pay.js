50b2af3cf442b16d34aaccfaac8ada6bc79b8c05
a.baraliu <baraliualbina@gmail.com>
2024-07-30 10:32:38 +0200

    30/07 update

--- view/frontend/web/js/view/checkout/ideal-fast-checkout/ideal-pay.js
+++ view/frontend/web/js/view/checkout/ideal-fast-checkout/ideal-pay.js
@@ -1,113 +1,89 @@
-/**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-define(
-    [
-        'Magento_Checkout/js/model/quote',
-        'Magento_Checkout/js/model/url-builder',
-        'mage/storage',
-        'mage/url',
-        'Magento_Checkout/js/model/error-processor',
-        'Magento_Customer/js/model/customer',
-        'Magento_Checkout/js/model/full-screen-loader',
-        'Magento_CheckoutAgreements/js/model/agreements-assigner',
-        'jquery',
-        'Magento_Ui/js/modal/alert',
-        'mage/translate'
-    ],
-    function (
-        quote,
-        urlBuilder,
-        storage,
-        url,
-        errorProcessor,
-        customer,
-        fullScreenLoader,
-        agreementsAssigner,
-        $,
-        alert,
-        $t
-    ) {
-        'use strict';
+define([
+    'jquery',
+    'uiComponent',
+    'ko',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/model/url-builder',
+    'mage/storage',
+    'mage/url',
+    'Magento_Checkout/js/model/error-processor',
+    'Magento_Customer/js/model/customer',
+    'Magento_Checkout/js/model/full-screen-loader',
+    'Magento_CheckoutAgreements/js/model/agreements-assigner',
+    'Magento_Ui/js/modal/alert',
+    'mage/translate',
+    'buckaroo/checkout/common'
+], function (
+    $,
+    Component,
+    ko,
+    quote,
+    urlBuilder,
+    storage,
+    url,
+    errorProcessor,
+    customer,
+    fullScreenLoader,
+    agreementsAssigner,
+    alert,
+    $t,
+    checkoutCommon
+) {
+    'use strict';
 
-        // console.log("HEREEqqqq");
-        return function (paymentData, redirectOnSuccess, messageContainer) {
-            var serviceUrl,
-                payload;
+    return Component.extend({
+        defaults: {
+            template: 'Buckaroo_Magento2/catalog/product/view/ideal-fast-checkout'
+        },
+        initialize: function () {
+            this._super();
+            console.log('Component initialized');
+        },
+        startProcess: function() {
+            console.log('startProcess called');
+            this.onShippingChangeHandler();
+            this.createPaymentHandler();
+            this.processPayment();
+        },
+        processPayment: function (paymentData, redirectOnSuccess, messageContainer) {
+            console.log('processPayment called');
+            var serviceUrl, payload;
 
-            // console.log("AAAAA");
-            // console.log(paymentData);
             redirectOnSuccess = redirectOnSuccess !== false;
-            // console.log("BBBB");
-            // agreementsAssigner(paymentData);
-
-            // console.log("Processing Buckaroo iDEAL payment");
-
-            // paymentData["method"] = "buckaroo_magento2_ideal";
-
-
 
             paymentData = {
                 method: "buckaroo_magento2_ideal",
-                // cartId: "cJ9ZIr1cJZdIygX8R1V3guLH5m0fSFWc",
                 po_number: null,
-                additional_data:
-                    {issuer: "ASNBNL21"}
-
+                additional_data: { issuer: "ASNBNL21" }
             };
-            /**
-             * Checkout for guest and registered customer.
-             */
+
             if (!customer.isLoggedIn()) {
-                // console.log(quote);
-                serviceUrl = urlBuilder.createUrl(
-                    '/guest-buckaroo/:quoteId/payment-information',
-                    {
-                        // quoteId: quote.getQuoteId()
-                        quoteId: "7N8n502viMwTWodLDEG3wymEJgJOGB80"
-                    }
-                );
-                // console.log("55555");
+                serviceUrl = urlBuilder.createUrl('/guest-buckaroo/:quoteId/payment-information', {
+                    quoteId: "23WJG3msarGt0AzdP6mEvpWp2AFcM4Iw"
+                });
                 payload = {
-                    cartId: "7N8n502viMwTWodLDEG3wymEJgJOGB80",
+                    cartId: "23WJG3msarGt0AzdP6mEvpWp2AFcM4Iw",
                     email: "albina@random.com",
                     paymentMethod: paymentData
                 };
-                // console.log("5656");
-                // console.log(payload);
-
             } else {
-                serviceUrl = urlBuilder.createUrl('/buckaroo/payment-information', {});
-                // console.log("Service URL:", serviceUrl);
+                serviceUrl = url.createUrl('/buckaroo/payment-information', {});
                 payload = {
                     cartId: quote.getQuoteId(),
                     paymentMethod: paymentData
                 };
             }
 
-            // console.log(serviceUrl);
+            serviceUrl = 'rest/default/V1/guest-buckaroo/23WJG3msarGt0AzdP6mEvpWp2AFcM4Iw/payment-information';
 
-
-
-            serviceUrl = 'rest/default/V1/guest-buckaroo/7N8n502viMwTWodLDEG3wymEJgJOGB80/payment-information'
-
-            // console.log("TESSSST")
-            // console.log(serviceUrl)
-
-            // fullScreenLoader.startLoader();
-
-            // console.log("8888");
-            // console.log(payload);
             return storage.post(
                 serviceUrl,
                 JSON.stringify(payload)
             ).done(
                 function (response) {
-                    // console.log("999");
+                    console.log('Payment processed successfully', response);
                     let jsonResponse = $.parseJSON(response);
                     if (typeof jsonResponse === 'object' && typeof jsonResponse.limitReachedMessage === 'string') {
-                        // console.log("GGGGGGGGGGG");
                         alert({
                             title: $t('Error'),
                             content: $t(jsonResponse.limitReachedMessage),
@@ -121,21 +97,97 @@ define(
                         });
                         $('.' + paymentData.method).remove();
                     } else if (redirectOnSuccess) {
-                        console.log("SSSSSSSS")
+                        checkoutCommon.redirectHandle(jsonResponse);
                         window.location.replace(url.build('checkout/onepage/success/'));
                     }
                     window.checkoutConfig.payment.buckaroo.response = response;
+                    checkoutCommon.redirectHandle(jsonResponse);
+
                     fullScreenLoader.stopLoader();
                 }
             ).fail(
                 function (response) {
-                    // console.log("VVVVV");
-                    // console.log(response);
-                    // console.log("VVVVV");
+                    console.log('Payment processing failed', response);
                     errorProcessor.process(response, messageContainer);
                     fullScreenLoader.stopLoader();
                 }
             );
-        };
-    }
-);
+        },
+        onShippingChangeHandler: function(data, actions) {
+            console.log('onShippingChangeHandler called');
+
+            if (
+                this.page === 'product' &&
+                $("#product_addtocart_form").valid() === false
+            ) {
+                return actions.reject();
+            }
+
+            let shipping = this.setShipping(data);
+            return new Promise((resolve, reject) => {
+                shipping.then(
+                    (response) => {
+                        console.log('Shipping set successfully', response);
+                        if (!response.message) {
+                            this.options.amount = response.value;
+                            this.cart_id = response.cart_id;
+                            actions.order.patch([
+                                {
+                                    op: "replace",
+                                    path: "/purchase_units/@reference_id=='default'/amount",
+                                    value: response,
+                                },
+                            ]).then((resp) => resolve(resp), (err) => reject(err));
+                        } else {
+                            reject(response.message);
+                        }
+                    },
+                    () => {
+                        reject($t("Cannot create payment"));
+                    }
+                );
+
+            });
+        },
+        createPaymentHandler: function(data) {
+            console.log('createPaymentHandler called');
+            console.log(data);
+            return this.createTransaction(data);
+        },
+        createTransaction: function(orderId) {
+            console.log('createTransaction called');
+
+            const cart_id = this.cart_id;
+            return new Promise((resolve, reject) => {
+                $.post(url.build("rest/V1/buckaroo/paypal-express/order/create"),
+                    {
+                        paypal_order_id: 5,
+                        cart_id
+                    }).then(
+                    (response) => {
+                        console.log('Transaction created successfully', response);
+                        this.result = response;
+                        resolve(response);
+                    },
+                    (reason) => reject(reason)
+                );
+            });
+        },
+
+        getOrderData() {
+            let form = $("#product_addtocart_form");
+            if (this.page === 'product') {
+                return form.serialize();
+            }
+        },
+        setShipping: function(data) {
+            console.log('setShipping called');
+
+            return $.post(url.build("rest/V1/buckaroo/paypal-express/quote/create"), {
+                shipping_address: "ddddd",
+                order_data: this.getOrderData(),
+                page: this.page,
+            });
+        }
+    });
+});
