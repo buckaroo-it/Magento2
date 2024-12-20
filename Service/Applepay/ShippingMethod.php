fb3f57e9c970be91073a1d5054e09dc5d3ba38d2
a.baraliu <baraliualbina@gmail.com>
2024-12-20 13:39:32 +0100

    changed shipping method

--- Service/Applepay/ShippingMethod.php
+++ Service/Applepay/ShippingMethod.php
@@ -9,6 +9,7 @@ use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Quote\Api\Data\ShippingMethodInterface;
 use Magento\Quote\Model\Cart\ShippingMethodConverter;
 use Magento\Quote\Model\Quote\TotalsCollector;
+use Buckaroo\Magento2\Logging\Log as BuckarooLog;
 
 class ShippingMethod
 {
@@ -28,44 +29,67 @@ class ShippingMethod
     private TotalsCollector $totalsCollector;
 
     /**
+     * @var BuckarooLog
+     */
+    private BuckarooLog $logger;
+
+    /**
      * @param ExtensibleDataObjectConverter $dataObjectConverter
      * @param ShippingMethodConverter $shippingMethodConverter
      * @param TotalsCollector $totalsCollector
+     * @param BuckarooLog $logger
      */
     public function __construct(
         ExtensibleDataObjectConverter $dataObjectConverter,
         ShippingMethodConverter $shippingMethodConverter,
-        TotalsCollector $totalsCollector
+        TotalsCollector $totalsCollector,
+        BuckarooLog $logger
     ) {
         $this->dataObjectConverter = $dataObjectConverter;
         $this->shippingMethodConverter = $shippingMethodConverter;
         $this->totalsCollector = $totalsCollector;
+        $this->logger = $logger;
     }
 
     public function getAvailableMethods($cart)
     {
-        $address = $cart->getShippingAddress();
+        $this->logger->addDebug('Starting getAvailableMethods process.');
 
-        $address->setLimitCarrier(null);
+        $address = $cart->getShippingAddress();
+        //$address->setLimitCarrier(null);
+
+        $this->logger->addDebug('address:::::'. json_decode($address));
         $address->setQuote($cart);
         $address->setCollectShippingRates(true);
-        $this->totalsCollector->collectAddressTotals($cart, $address);
-        $methods = [];
 
+        try {
+            $this->totalsCollector->collectAddressTotals($cart, $address);
+        } catch (\Exception $e) {
+            $this->logger->addError('Error collecting address totals: ' . $e->getMessage());
+            throw new LocalizedException(__('Unable to collect shipping rates.'));
+        }
+
+        $methods = [];
         $shippingRates = $address->getGroupedAllShippingRates();
+
+        $this->logger->addDebug('shipping rates:::::'. json_decode($shippingRates));
+
         foreach ($shippingRates as $carrierRates) {
             foreach ($carrierRates as $rate) {
-                $methodData = $this->dataObjectConverter->toFlatArray(
-                    $this->shippingMethodConverter->modelToDataObject($rate, $cart->getQuoteCurrencyCode()),
-                    [],
-                    ShippingMethodInterface::class
-                );
+//                $methodData = $this->dataObjectConverter->toFlatArray(
+//                    $this->shippingMethodConverter->modelToDataObject($rate, $cart->getQuoteCurrencyCode()),
+//                    [],
+//                    ShippingMethodInterface::class
+//                );
                 $methods[] = $this->processMoneyTypeData(
-                    $methodData,
+                    $rate,
                     $cart->getQuoteCurrencyCode()
                 );
             }
         }
+
+        $this->logger->addDebug('Shipping methods retrieved successfully.');
+
         return $methods;
     }
     private function processMoneyTypeData(array $data, string $quoteCurrencyCode): array
