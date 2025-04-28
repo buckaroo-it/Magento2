876b0426b6950958401ada47fd6772c51ae82e51
vegimcarkaxhija <vegim.carkaxhija@gmail.com>
2025-04-28 14:55:15 +0200

    refactor Push.php: add plugin to fix PPE status code retrieval

---
+++ Plugin/FixPPEStatusCode.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Buckaroo\Magento2\Plugin;
+
+class FixPPEStatusCode
+{
+    public function aroundGetStatusCode(
+        \Buckaroo\Magento2\Model\Push $subject,
+        callable $proceed
+    ) {
+        // Let the original method try first
+        $result = $proceed();
+
+        // If it failed to pick up the code (0) but the push does have brq_statuscode,
+        // fall back to that value.
+        if ($result === 0 && isset($subject->postData['brq_statuscode'])) {
+            $result = $subject->postData['brq_statuscode'];
+        }
+
+        return $result;
+    }
+}
