a264d3aa26562f5dde79c786f7df0d9791d9e2e5
vegimcarkaxhija <vegim.carkaxhija@gmail.com>
2025-07-11 10:37:00 +0200

    fix: correct calculation of already paid amount in checkout summary

--- Model/Total/Quote/BuckarooRemainingAmount.php
+++ Model/Total/Quote/BuckarooRemainingAmount.php
@@ -50,6 +50,7 @@ class BuckarooRemainingAmount extends AbstractTotal
      * @param Quote $quote
      * @param Total $total
      * @return array
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function fetch(Quote $quote, Total $total)
     {
