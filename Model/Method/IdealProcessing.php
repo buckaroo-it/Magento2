3b8f3248a742b258707af0a398b31260b4fef9b9
a.baraliu <baraliualbina@gmail.com>
2025-07-29 13:42:06 +0200

    Add availability check for payment method based on order status

--- Model/Method/IdealProcessing.php
+++ Model/Method/IdealProcessing.php
@@ -130,4 +130,19 @@ class IdealProcessing extends AbstractMethod
     {
         return $this->buckarooPaymentMethodCode;
     }
+
+    public function isAvailable(\Magento\Quote\Api\Data\CartInterface $quote = null)
+    {
+        $orderId = $quote ? $quote->getReservedOrderId() : null;
+
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        $paymentGroupTransaction = $objectManager->get(\Buckaroo\Magento2\Helper\PaymentGroupTransaction::class);
+
+        if ($paymentGroupTransaction->getAlreadyPaid($orderId) > 0) {
+            return false;
+        }
+
+        return true;
+    }
 }
