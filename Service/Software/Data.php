c4e13cfa38958780282a1ef9e76338a89399f302
vegimcarkaxhija <vegim.carkaxhija@gmail.com>
2025-09-17 15:52:30 +0200

    update: bump module version to v1.53.1 in module.xml, Data.php, and composer.json

--- Service/Software/Data.php
+++ Service/Software/Data.php
@@ -36,7 +36,7 @@ class Data
     const MODULE_CODE = 'Buckaroo_Magento2';
 
     /** Version of Module */
-    const BUCKAROO_VERSION = '1.53.0';
+    const BUCKAROO_VERSION = '1.53.1';
 
     /** @var ProductMetadataInterface */
     private $productMetadata;
