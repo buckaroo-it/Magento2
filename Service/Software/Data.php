b2959867195ee9f6f3eb5c92bd0037fd18794529
vegimcarkaxhija <vegim.carkaxhija@gmail.com>
2025-10-30 15:53:50 +0100

    update: bump module version to 1.54.0 for compatibility with PHP 8.4

--- Service/Software/Data.php
+++ Service/Software/Data.php
@@ -38,7 +38,7 @@ class Data
     public const MODULE_CODE = 'Buckaroo_Magento2';
 
     /** Version of Module */
-    public const BUCKAROO_VERSION = '1.53.2';
+    public const BUCKAROO_VERSION = '1.54.0';
 
     /** @var ProductMetadataInterface */
     private $productMetadata;
