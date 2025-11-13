235c272ac953548b8d875d797f95fc1c4ce566ce
vegimcarkaxhija <vegim.carkaxhija@gmail.com>
2025-11-13 15:05:23 +0100

    chore: update module version to 2.1.0

--- Service/Software/Data.php
+++ Service/Software/Data.php
@@ -38,7 +38,7 @@ class Data
     public const MODULE_CODE = 'Buckaroo_Magento2';
 
     /** Version of Module */
-    public const BUCKAROO_VERSION = '2.0.3';
+    public const BUCKAROO_VERSION = '2.1.0';
 
     /**
      * @var ProductMetadataInterface
--- Setup/Patch/Data/SetupModuleDataPatch.php
+++ Setup/Patch/Data/SetupModuleDataPatch.php
@@ -452,7 +452,7 @@ class SetupModuleDataPatch implements DataPatchInterface
      */
     public static function getVersion()
     {
-        return '2.0.3';
+        return '2.1.0';
     }
 
     /**
