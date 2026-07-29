b60de98cd1d95f25b2ec4d9b12d76e7ac345baf5
Sander <sander.vdhulst98@gmail.com>
2026-07-29 09:50:23 +0200

    BTI-1217-Klarna capture fails if the order is partially paid with a giftcard (related to BTI-980)

--- Gateway/Request/Articles/ArticlesHandler/AfterpayHandler.php
+++ Gateway/Request/Articles/ArticlesHandler/AfterpayHandler.php
@@ -228,8 +228,7 @@ class AfterpayHandler extends AbstractArticlesHandler
     public function getRewardLine()
     {
         try {
-            $quote = $this->getQuote();
-            $discount = (float)$quote->getRewardCurrencyAmount();
+            $discount = (float)$this->getOrder()->getData('reward_currency_amount');
 
             if ($discount <= 0) {
                 return [];
@@ -245,7 +244,7 @@ class AfterpayHandler extends AbstractArticlesHandler
                 0
             );
         } catch (\Error $e) {
-            $this->buckarooLog->addDebug(__METHOD__ . '|getRewardCurrencyAmount method not available - Adobe Commerce reward points may not be installed');
+            $this->buckarooLog->addDebug(__METHOD__ . '|reward_currency_amount not available - Adobe Commerce reward points may not be installed');
             return [];
         } catch (\Exception $e) {
             $this->buckarooLog->addError(__METHOD__ . '|Error getting reward points amount: ' . $e->getMessage());
@@ -261,8 +260,7 @@ class AfterpayHandler extends AbstractArticlesHandler
     public function getGiftCardLine(): array
     {
         try {
-            $quote = $this->getQuote();
-            $discount = (float)$quote->getGiftCardsAmount();
+            $discount = (float)$this->getOrder()->getData('gift_cards_amount');
 
             if ($discount <= 0) {
                 return [];
@@ -278,7 +276,7 @@ class AfterpayHandler extends AbstractArticlesHandler
                 0
             );
         } catch (\Error $e) {
-            $this->buckarooLog->addDebug(__METHOD__ . '|getGiftCardsAmount method not available - Adobe Commerce gift cards may not be installed');
+            $this->buckarooLog->addDebug(__METHOD__ . '|gift_cards_amount not available - Adobe Commerce gift cards may not be installed');
             return [];
         } catch (\Exception $e) {
             $this->buckarooLog->addError(__METHOD__ . '|Error getting gift card amount: ' . $e->getMessage());
